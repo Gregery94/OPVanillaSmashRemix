1e09f5c67eed20ec2060f4d0e5a3478e275625a1
Gregery94 <40548224+Gregery94@users.noreply.github.com>
2023-04-18 16:09:14 -0400

    fix compiler issue

--- src/OPLink/OPLink.asm
+++ src/OPLink/OPLink.asm
@@ -3,20 +3,20 @@
 
 scope OPLink {
     // Insert Moveset files
-    insert FSMASH,"moveset/FORWARD_SMASH.bin"
+    // insert ATTACK,"moveset/ATTACK.bin"
 
     // Modify Action Parameters             // Action               // Animation                // Moveset Data             // Flags
 
     // COPY ELINK
-    Character.edit_vanilla_action_parameters(LINK, Action.FSmash,   -1,                         Elink.FSMASH,               -1)
+    Character.edit_vanilla_action_parameters(LINK, Action.FSmash,   -1,                         ELink.FSMASH,               -1)
 
     // COPY JLINK
-    Character.edit_vanilla_action_parameters(LINK, Action.UTilt,           -1,                         JLink.UTILT,                -1)
-    Character.edit_vanilla_action_parameters(LINK, Action.USmash,          -1,                         JLink.USMASH,               -1)
-    Character.edit_vanilla_action_parameters(LINK, Action.AttackAirB,      -1,                         JLink.BAIR,                 -1)
-    Character.edit_vanilla_action_parameters(LINK, Action.AttackAirN,      -1,                         JLink.NAIR,                 -1)
-    Character.edit_vanilla_action_parameters(LINK, Action.AttackAirF,      -1,                         JLink.FAIR,                 -1)
-    Character.edit_vanilla_action_parameters(LINK, Action.AttackAirD,      -1,                         JLink.DAIR,                 -1)
+    Character.edit_vanilla_action_parameters(LINK, Action.UTilt,           -1,                  JLink.UTILT,                -1)
+    Character.edit_vanilla_action_parameters(LINK, Action.USmash,          -1,                  JLink.USMASH,               -1)
+    Character.edit_vanilla_action_parameters(LINK, Action.AttackAirB,      -1,                  JLink.BAIR,                 -1)
+    Character.edit_vanilla_action_parameters(LINK, Action.AttackAirN,      -1,                  JLink.NAIR,                 -1)
+    Character.edit_vanilla_action_parameters(LINK, Action.AttackAirF,      -1,                  JLink.FAIR,                 -1)
+    Character.edit_vanilla_action_parameters(LINK, Action.AttackAirD,      -1,                  JLink.DAIR,                 -1)
     
 
     // Modify Menu Action Parameters             // Action          // Animation                // Moveset Data             // Flags
