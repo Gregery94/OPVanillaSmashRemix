dea1e06c4bec9b936cc5b521a53290d3a6f956bb
Gregery94 <40548224+Gregery94@users.noreply.github.com>
2023-04-26 12:48:46 -0400

    yoshi, samus, puff
     - yoshi has one more jump
     - samus has two more jumps
     - puffs back air hitboxes are twice the size

--- src/OPJigglyPuff/OPJigglyPuff.asm
+++ src/OPJigglyPuff/OPJigglyPuff.asm
@@ -4,9 +4,11 @@
 scope OPJigglyPuff {
 
     // Insert Moveset files
+    JIGGLY_BAIR:
+    insert BACK_AIR,"moveset/BACK_AIR.bin"
 
     // Modify Action Parameters                             // Action           // Animation    // Moveset Data                 // Flags
-
+    Character.edit_vanilla_action_parameters(JIGGLYPUFF, Action.AttackAirB,     -1,                  BACK_AIR,                       -1)
     // Modify Menu Action Parameters             // Action          // Animation                // Moveset Data             // Flags
 
     // Modify Actions            // Action          // Staling ID   // Main ASM                 // Interrupt/Other ASM          // Movement/Physics ASM         // Collision ASM
