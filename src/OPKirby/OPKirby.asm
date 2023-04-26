f1f8f8549253a1033b0a07d2e9361079ebdd46e3
Gregery94 <40548224+Gregery94@users.noreply.github.com>
2023-04-26 12:27:43 -0400

    OP Luigi
     - JPN back throw
     - JPN fireball damage, knockback and longer duration
     - Aerial up special uses the JPN up special since its more suited for KOing opponents
     - Grounded up special combines the best of NA and JPN hitboxes

--- src/OPKirby/OPKirby.asm
+++ src/OPKirby/OPKirby.asm
@@ -6,7 +6,6 @@ scope OPKirby {
     // Insert Moveset files
 
     // Modify Action Parameters                             // Action           // Animation    // Moveset Data                 // Flags
-
     Character.edit_vanilla_action_parameters(KIRBY, Action.Jab2,           -1,                  JKirby.NEUTRAL2,                   -1)
     Character.edit_vanilla_action_parameters(KIRBY, Action.DSmash,         -1,                  JKirby.DSMASH,                     -1)
     Character.edit_vanilla_action_parameters(KIRBY, Action.USmash,         -1,                  JKirby.USMASH,                     -1)
@@ -16,6 +15,7 @@ scope OPKirby {
     Character.edit_vanilla_action_parameters(KIRBY, 0x10A,                 -1,                  JKirby.DOWN_SPECIAL_LANDING,       -1)
     Character.edit_vanilla_action_parameters(KIRBY, 0x10B,                 -1,                  JKirby.DOWN_SPECIAL_FALL_OFF,      -1)
 
+    // stone armour modified in kirbyshared.asm
 
     // Modify Menu Action Parameters             // Action          // Animation                // Moveset Data             // Flags
 
--- src/OPLuigi/OPLuigi.asm
+++ src/OPLuigi/OPLuigi.asm
@@ -5,14 +5,28 @@ scope OPLuigi {
 
     // Insert Moveset files
     insert DASH_ATTACK,"moveset/DASH_ATTACK.bin"
+    insert USPECIALAIR,"moveset/USPECIALAIR.bin"
 
-    // Modify Action Parameters                             // Action           // Animation    // Moveset Data                 // Flags
+    // Modify Action Parameters                       // Action            // Animation // Moveset Data                 // Flags
 
     // DASH ATTACK HITBOX FIX
-    Character.edit_vanilla_action_parameters(LUIGI,         Action.DashAttack,   -1,            DASH_ATTACK,                           -1)
+    Character.edit_vanilla_action_parameters(LUIGI,   Action.DashAttack,   -1,          DASH_ATTACK,                    -1)
+
+    // FROM JLUIGI
+    Character.edit_vanilla_action_parameters(LUIGI,   Action.ThrowB,       -1,          JLuigi.BTHROW,                  -1)
+    Character.edit_vanilla_action_parameters(LUIGI,   0xE1,                -1,          JLuigi.USPECIALGRND,            -1)
+
+    // USP AIR
+    Character.edit_vanilla_action_parameters(LUIGI,   0xE2,                -1,          USPECIALAIR,                    -1)
 
     // Modify Menu Action Parameters             // Action          // Animation                // Moveset Data             // Flags
 
     // Modify Actions            // Action          // Staling ID   // Main ASM                 // Interrupt/Other ASM          // Movement/Physics ASM         // Collision ASM
+
+    // @ Description
+    // Set Luigis fireball duration to be the same as JLuigi
+    OS.patch_start(0x1038A0, 0x80188E60)
+    dw 90
+    OS.patch_end()
 
 }
